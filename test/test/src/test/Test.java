4f4572daf18bb239391db8ff0dd63485f2ad4ad3
linjing <linjing1@lvmama.com>
2018-11-26 14:29:35 +0800

    first commit

---
+++ test/test/src/test/Test.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author linjing
+ * @date 2018年11月26日  
+ * @desc:
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
